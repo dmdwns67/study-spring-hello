7813e3572c3ca841672864f804ca2e6623714478
dmdwns67 <dmdwns67@naver.com>
2020-11-11 11:21:34 +0900

    api test

--- src/main/java/com/study/study/HelloController.java
+++ src/main/java/com/study/study/HelloController.java
@@ -1,9 +1,16 @@
 package com.study.study;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class HelloController {
@@ -20,6 +27,17 @@ public class HelloController {
         String str = helloService.getDemo();
         model.addAttribute("data", str);
         System.out.println("LOG: " + str);
+
         return "hello";
+    }
+
+    @PostMapping("hello2")
+    public ResponseEntity create(){
+        Map<String, String> map = new HashMap<>();
+        String str = helloService.getDemo();
+        map.put("data", str);
+        System.out.println("TEST: " + str);
+
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 }
